75022c7d1500ce9f5fe34231d48317175f539d07
Bianca Trif <bianca.trif28@gmail.com>
2017-04-30 08:33:17 +0100

    Connected Link and data to Main component

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import List from './List';
+import data from './mocked-data';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            shortageList: data.shortages,
+            surplusList: data.surpluses
+        }
+    }
     render() {
+        const { surplusList, shortageList } = this.state;
         return (
-            <div>
-                Main page
+            <div className="grid">
+                <List list={shortageList.list} tag={shortageList.tag} />
+                <List list={surplusList.list} tag={surplusList.tag} />
             </div>
         );
     }
