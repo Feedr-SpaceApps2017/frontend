afc8e229dd24f8e62609ab23c6e4160001176f99
Bianca Trif <bianca.trif28@gmail.com>
2017-04-30 08:32:07 +0100

    Added mocked data for global lists

---
+++ src/mocked-data.js
@@ -0,0 +1,60 @@
+const data = {
+    shortages: {
+        tag: "Shortages",
+        list: [{
+            rank: 1,
+            name: 'Avocado'
+        }, {
+            rank: 2,
+            name: 'Courgette'
+        }, {
+            rank: 3,
+            name: 'Oranges'
+        }, {
+            rank: 4,
+            name: 'Agrarian'
+        }, {
+            rank: 5,
+            name: 'Cocoa'
+        }, {
+            rank: 6,
+            name: 'Lettuce'
+        }]
+    },
+    surpluses: {
+        tag: "Surpluses",
+        list: [{
+            rank: 1,
+            name: 'Wheat'
+        }, {
+            rank: 2,
+            name: 'Rice'
+        }, {
+            rank: 3,
+            name: 'Maize'
+        }, {
+            rank: 4,
+            name: 'Soy beans'
+        }, {
+            rank: 5,
+            name: 'Barley'
+        }, {
+            rank: 6,
+            name: 'Sorghum'
+        }, {
+            rank: 7,
+            name: 'Millet'
+        }, {
+            rank: 8,
+            name: 'Groundnut'
+        }, {
+            rank: 9,
+            name: 'Beans'
+        }, {
+            rank: 10,
+            name: 'Rapeseed'
+        }]
+    }
+};
+
+export default data;
